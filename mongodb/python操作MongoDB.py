62338e95453171773185f7477b6c5975c4e94ae5
WeiLai <imwl@live.com>
2019-12-06 17:04:10 +0800

    Update python操作MongoDB.py

--- mongodb/python操作MongoDB.py
+++ mongodb/python操作MongoDB.py
@@ -8,8 +8,21 @@ class Test_Mongo():
     client:dict = MongoClient()
     db:dict = client['test']  # 连接到 test 数据库
     
+    def add_one(self):
+        post = {
+            'name': 'justin',
+            'age': 18,
+            'sex': 'male',
+            'grade': 80
+        }
+        return self.db.students.insert_one(post)
+
+    def add_many(self):
+        return self.db.students.insert_many([{'name':i} for i in range(0,10)])
+
+
     def get_one(self):
-        return self.db.students.find_one({'sex':'male'})
+        return self.db.students.find_one({'name':'justin'})
 
     def get_more(self):
         return self.db.students.find({'sex':'male'})
@@ -18,14 +31,39 @@ class Test_Mongo():
         obj = ObjectId(oid)
         return self.db.students.find_one({'_id': obj})
 
+    def get_count(self):
+        return self.db.students.estimated_document_count()
+
+
+    def update_one(self):
+        return self.db.students.update_one({'name':'justin'},{'$inc':{'age':10}})
+
+    def update_many(self):
+        return self.db.students.update_many({},{'$inc':{'age':5}})
+
+    def delete_one(self):
+        return self.db.students.delete_one({'name':'justin'})
+
+    def delete_many(self):
+        return self.db.students.delete_many({'name':'justin'})
+
 
 def main():
     obj = Test_Mongo()
+    print(obj.add_one().inserted_id)
     print(obj.get_one())
+    print(obj.add_many().inserted_ids)
+    print(obj.get_count())
     for item in obj.get_more():
         print(item['_id'])
-    print(obj.get_one_from_oid('5de8b35c75deb540ecfc5340'))
+    print(obj.get_one_from_oid(obj.add_one().inserted_id))
+    print(obj.update_one().matched_count)
+    print(obj.update_one().matched_count)
+    print(obj.update_many().matched_count)
+    print(obj.update_many().matched_count)
+    print(obj.delete_one().deleted_count)
+    print(obj.delete_many().deleted_count)
+
 
 if __name__ == '__main__':
     main()
-
