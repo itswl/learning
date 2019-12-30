930c540be0acbc46941b69d886d902c5cfa923ef
WeiLai <imwl@live.com>
2019-12-30 12:08:24 +0800

    Update is_json.py

--- python/is_json.py
+++ python/is_json.py
@@ -5,25 +5,35 @@ import sys
 import os
 import glob
 
-script_path = os.path.split(os.path.realpath(__file__))[0]
-def is_json(json_file):
-    try:
-        with open(json_file,'r') as f:
-            load_dict = json.load(f)
-            print(json_file +  ' True')
-    except Exception as e:
-        print(json_file +  ' ERROR')
-        print(e)
+class Is_json:
+    path = os.path.split(os.path.realpath(__file__))[0]
 
-def json_list():
-    try:    
-        json_name = sys.argv[1]
-        json_list = []       
-        for json_name in sys.argv[1:]:
-            json_list.append(json_name)
-            
-    except:   
-        json_list = glob.glob(os.path.join(script_path,'*.json'))
-    return json_list
 
-[is_json(json_file) for json_file in json_list()]
+    def is_json(self, json_file):
+        try:
+            with open(json_file,'r') as f:
+                load_dict = json.load(f)
+                print(json_file +  ' True')
+        except Exception as e:
+            print(json_file +  ' ERROR')
+            print(e)
+
+    @property
+    def json_list(self):
+        try:    
+            json_name = sys.argv[1]
+            json_list = []       
+            for json_name in sys.argv[1:]:
+                json_list.append(json_name)           
+        except:   
+            json_list = glob.glob(os.path.join(self.path,'*.json'))
+        return json_list
+
+    @classmethod
+    def run(cls):
+        for json_file in cls().json_list:
+            cls().is_json(json_file)
+
+if __name__ == '__main__':
+    Is_json.run()
+
